0e254b6f1f1f4e486fc1832d2ea1ad30552e06cc
Tiago Alves de Oliveira <tiagofga@gmail.com>
2026-03-11 19:15:17 -0300

    Fix gitignore to track public mlp headers for CI build

---
+++ include/mlp/types.hpp
@@ -0,0 +1,13 @@
+#ifndef MLP_TYPES_HPP
+#define MLP_TYPES_HPP
+
+#include <vector>
+
+namespace mlp {
+
+using Vector = std::vector<double>;
+using Matrix = std::vector<Vector>;
+
+}  // namespace mlp
+
+#endif
